730556e9ebc5cbaaddffffdaeb0ede0d5a4a3d76
Adam Basila <adamcabasila@gmail.com>
2018-10-05 16:40:18 -0600

    initailize repo

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id='app'>
+    <main>
+      <p :listings="listings"></p>
+    </main>
+  </div>
+</template>
+
+<script>
+  export default {
+      name:'app',
+
+    data(){
+      return{
+        lisings: [],
+        apiURL: ''
+      }
+    },
+    mounted (){
+      fetch(this.apiURL)
+        .then(response => response.json())
+        .then(response => {
+        this.listings = response
+      })
+    },
+  }
+
+</script>
